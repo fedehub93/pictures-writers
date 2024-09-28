4cc6fe3f8674ec9a3fe134895b587662c0811cf2
Federico Verrengia <federico@standupx.com>
2024-09-28 12:54:29 +0200

    feat: added 2 pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -90,6 +90,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "yearly",
       priority: 0.5,
     },
+    {
+      url: "https://pictureswriters.com/feedback-gratuito-sceneggiatura",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://pictureswriters.com/policy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
     ...posts,
     ...categories,
     ...tags,
