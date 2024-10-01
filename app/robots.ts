f8a1c30077440f465fce8b9232d9de0b494c4eeb
Federico Verrengia <federico@standupx.com>
2024-10-01 15:05:13 +0200

    feat: turn off indexing

--- app/robots.ts
+++ app/robots.ts
@@ -4,15 +4,16 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/"],
-      disallow: [
-        "/search?q=",
-        "/admin/",
-        "/confirm-subscription",
-        "/cancel-subscription",
-        "/download-ebooks",
-        "/cos-e-il-linguaggio-cinematografico-scrivere-per-il cinema",
-      ],
+      disallow: "/",
+      // allow: ["/"],
+      // disallow: [
+      //   "/search?q=",
+      //   "/admin/",
+      //   "/confirm-subscription",
+      //   "/cancel-subscription",
+      //   "/download-ebooks",
+      //   "/cos-e-il-linguaggio-cinematografico-scrivere-per-il cinema",
+      // ],
     },
     sitemap: ["https://pictureswriters.com/sitemap.xml"],
   };
