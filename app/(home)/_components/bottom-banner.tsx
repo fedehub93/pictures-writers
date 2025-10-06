8ebbd9f9f9555f2a5de19990515f1b5466391354
Federico Verrengia <federico@standupx.com>
2025-10-06 15:23:55 +0200

    fix: fixed path in bottom banner

--- app/(home)/_components/bottom-banner.tsx
+++ app/(home)/_components/bottom-banner.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { Route } from "next";
+
 import Link from "next/link";
 
 export const BottomBanner = () => {
+  const path =
+    "/laboratorio-di-scrittura-di-un-soggetto-cinematografico-2025/" as Route;
   return (
     <div className=" w-full px-4 text-center py-1 md:py-4 border-b bg-accent fixed bottom-0">
       📚 <strong>Laboratorio di scrittura di un soggetto</strong> – 5 posti
       disponibili.
       <Link
-        href="/laboratorio-di-scrittura-di-un-soggetto-cinematografico-2025/"
+        href={path}
         type="button"
         role="button"
         className="font-semibold ml-1"
